94e7a73505cf1db2f581666bc09d3955ba7c7386
toxicGuru <besartmahmuti5@gmail.com>
2023-03-01 22:47:20 +0100

    add snnipet updated

--- src/pages/Snippet/AddSnippet/AddSnippet.tsx
+++ src/pages/Snippet/AddSnippet/AddSnippet.tsx
@@ -1,31 +1,31 @@
 import SyntaxHighlighter from 'react-syntax-highlighter';
 import withHeaderAndFooter from '../../../hoc/withHeaderAndFooter';
-import { github } from 'react-syntax-highlighter/dist/esm/styles/hljs';
+// import { github } from 'react-syntax-highlighter/dist/esm/styles/hljs';
+import { darcula } from 'react-syntax-highlighter/dist/esm/styles/hljs';
+import prettier from 'prettier/standalone';
+import parserBabel from 'prettier/parser-babel';
 
-import { useState } from 'react';
+// import { useEffect, useRef, useState } from 'react';
 const AddSnippet = () =>{
-
-    console.log(SyntaxHighlighter.supportedLanguages)
-    const [codeString, setCodeString] = useState('')
-    const handleCodeChange = (event: any) => {
-      setCodeString('')
-      setCodeString(event.target.innerText);
-    };
-    return(
-      
-        <div
-        className='container'
-        contentEditable={true}
-        onInput={handleCodeChange}
-        style={{ fontFamily: 'monospace', whiteSpace: 'pre-wrap',  minHeight: '100px' }}
-      >
-        
-        <SyntaxHighlighter language="javascript" style={github}>
-        {`function greet(name) {
+  const codeString = `function greet(name) {
     console.log(\`Hello, \${name}!\`);
-  }`}
-        </SyntaxHighlighter>
-      </div>
-    )
+  }`;
+
+  const formattedCode = prettier.format(codeString, {
+    parser: 'babel',
+    plugins: [parserBabel],
+    printWidth: 80,
+    tabWidth: 2,
+    useTabs: false,
+    semi: true,
+    singleQuote: true,
+    trailingComma: 'es5',
+  });
+
+  return (
+    <SyntaxHighlighter language="javascript" style={darcula} contentEditable>
+      {formattedCode}
+    </SyntaxHighlighter>
+  );
 }
 export default withHeaderAndFooter(AddSnippet)
